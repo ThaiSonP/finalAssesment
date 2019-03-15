3c8d7c25a93d3588bfbce356781c37ead9b5fc82
ThaiSonP <tysonpan@pursuit.org>
2019-03-15 11:09:10 -0400

    all tables have been created

--- Earworm_Report/backend/database/data.sql
+++ Earworm_Report/backend/database/data.sql
@@ -3,8 +3,35 @@ CREATE DATABASE earworm;
 
 \c earworm;
 
-CREATE TABLE Users(
+CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   username VARCHAR NOT NULL,
   UNIQUE (username)
+);
+
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  genre_name VARCHAR NOT NULL,
+  UNIQUE (genre_name)
+);
+
+CREATE TABLE songs(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR,
+  img_url VARCHAR,
+  user_id INT REFERENCES users(id),
+  genre_id INT REFERENCES genres (id)
+);
+
+CREATE TABLE favorites(
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  genre_id INT REFERENCES genres(id)
+);
+
+CREATE TABLE comments(
+  id SERIAL PRIMARY KEY,
+  comment_body VARCHAR,
+  user_id INT REFERENCES users(id),
+  song_id INT REFERENCES songs(id)
 )
