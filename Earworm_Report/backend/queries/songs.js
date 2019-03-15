f4af057ac7b550e34e7399bd12612fb0d2452b2e
ThaiSonP <tysonpan@pursuit.org>
2019-03-15 16:50:52 -0400

    get one random song added & tested

--- Earworm_Report/backend/queries/songs.js
+++ Earworm_Report/backend/queries/songs.js
@@ -19,6 +19,63 @@ const getAllSongs = (req,res)=>{
   })
 }
 
+const getSongsByGenre = (req,res)=>{
+  const genreId = req.params.genreId
+  db.any(
+    'SELECT * '+
+    'FROM songs '+
+    'JOIN genres '+
+    'ON songs.genre_id = genres.id '+
+    'WHERE genres.id = $1 ',genreId
+  )
+  .then(results=>{
+    res.status(200)
+    .json({
+      message: 'these are all the songs for THIS genre',
+      songs: results
+    })
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+
+const getSongsByUser = (req,res)=>{
+  const userId = req.params.userId
+  db.any(
+    'SELECT * FROM songs WHERE user_id=$1',userId
+  )
+  .then(results=>{
+    res.status(200)
+    .json({
+      message:'these are all the songs for THIS user',
+      songs:results
+    })
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+
+const getOneSong = (req,res)=>{
+  db.any(
+    'SELECT * '+
+    'FROM songs '+
+    'ORDER BY RANDOM() '+
+    'LIMIT 1'
+  ).then(result=>{
+    res.status(200)
+    .json({
+      message:'this is ONE randomly slected song',
+      song: result
+    })
+  }).catch(err=>{
+    console.log(err)
+  })
+
+}
+
 module.exports={
-  getAllSongs
+  getAllSongs,
+  getSongsByGenre,
+  getSongsByUser,
+  getOneSong
 }
--- Earworm_Report/backend/routes/songs.js
+++ Earworm_Report/backend/routes/songs.js
@@ -5,5 +5,8 @@ const db = require ('../queries/songs')
 
 // http://localhost:3009/songs/
 router.get('/',db.getAllSongs)
+router.get('/genres/:genreId',db.getSongsByGenre)
+router.get('/user/:userId',db.getSongsByUser)
+router.get('/random',db.getOneSong)
 
 module.exports = router
