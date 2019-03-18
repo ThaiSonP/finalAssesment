abef3700b249c5d839412b4c9b38de945754b302
ThaiSonP <tysonpan@pursuit.org>
2019-03-18 17:22:30 -0400

    update genre css

--- Earworm_Report/frontend/src/Songs/Genre.js
+++ Earworm_Report/frontend/src/Songs/Genre.js
@@ -1,18 +1,38 @@
 import React,{Component} from 'react'
 import '../styling/Songs.css'
+import axios from 'axios'
+import DisplaySongs from './DisplaySongs'
 
 class Genre extends Component {
   constructor(props){
     super(props)
     this.state={
+      genres:[],
       songs:[]
     }
   }
 
+  componentDidMount=()=>{
+    axios.get('/genres')
+    .then(genres=>{
+      this.setState({
+        genres:genres.data.genres})
+    })
+    axios.get('/songs')
+    .then(songs=>{
+      this.setState({
+        songs:songs.data.songs
+      })
+    })
+  }
+
   render (){
+    const{songs,genres}=this.state
+      console.log(this.state)
     return(
       <div className = 'songs'>
         <p>This is the Songs By Genre Page</p>
+        <DisplaySongs songs={songs}/>
       </div>
     )
   }
