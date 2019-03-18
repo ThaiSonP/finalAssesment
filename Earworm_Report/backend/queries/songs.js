a08b479c32569ebc050816a2cb3fb819a93d4863
ThaiSonP <tysonpan@pursuit.org>
2019-03-18 06:48:26 -0400

    added a new query to handle songs by posting date (id)

--- Earworm_Report/backend/queries/songs.js
+++ Earworm_Report/backend/queries/songs.js
@@ -1,17 +1,33 @@
 const db = require ('../database/index')
 
-const getAllSongs = (req,res)=>{
+const getPopularSongs = (req,res)=>{
   db.any(
     'SELECT songs.id,songs.title,songs.img_url,songs.user_id,songs.genre_id, COUNT (favorites.song_id) '+
     'FROM songs '+
     'JOIN favorites '+
     'ON songs.id = favorites.song_id '+
     'GROUP BY songs.id,songs.title,songs.img_url,songs.user_id,songs.genre_id '+
-    'ORDER BY COUNT desc '
+    'ORDER BY count DESC '
   ).then(response=>{
     res.status(200)
     .json({
       message: 'these are ALL THE SONGS SORTED BY FAVORITES',
+      songs:response
+    })
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+
+const getSongsByDate = (req,res)=>{
+  db.any(
+    'SELECT * '+
+    'FROM songs '+
+    'ORDER BY id DESC'
+  ).then(response=>{
+    res.status(200)
+    .json({
+      message: 'these are all the songs via latest first',
       songs:response
     })
   }).catch(err=>{
@@ -105,7 +121,8 @@ const deleteSong = (req,res)=>{
 }
 
 module.exports={
-  getAllSongs,
+  getPopularSongs,
+  getSongsByDate,
   getSongsByGenre,
   getSongsByUser,
   getOneSong,
--- Earworm_Report/backend/routes/songs.js
+++ Earworm_Report/backend/routes/songs.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const db = require ('../queries/songs')
 
 // http://localhost:3009/songs/
-router.get('/',db.getAllSongs)
+router.get('/popular',db.getPopularSongs)
+router.get('/',db.getSongsByDate)
 router.get('/genres/:genreId',db.getSongsByGenre)
 router.get('/user/:userId',db.getSongsByUser)
 router.get('/random',db.getOneSong)
