56adda93dab8043d4bebdf2eaea2110371cbe418
ThaiSonP <tysonpan@pursuit.org>
2019-03-21 13:28:39 -0400

    post and favorites button work

--- Earworm_Report/frontend/src/Home/Profile.js
+++ Earworm_Report/frontend/src/Home/Profile.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import PostSongs from './PostSongs'
 import axios from 'axios'
+import DisplaySongs from '../Songs/DisplaySongs'
 
 class Profile extends Component {
   constructor(props){
@@ -10,37 +11,60 @@ class Profile extends Component {
       currentUser:+props.match.params.id,
       display:true,
       songs:[],
-      favorites:[]
+      favorites:[],
+      selected:[]
     }
   }
 
-  componentDidMount(){
-    const {currentUser}=this.state
+  componentDidMount (){
+    const {currentUser,songs}=this.state
 
-    axios.get(`/songs/user/${currentUser}`)
+     axios.get(`/songs/user/${currentUser}`)
     .then(result=>{
       this.setState({
-        songs:result.data.songs
+        songs:result.data.songs,
+        selected:result.data.songs
       })
     })
 
-    axios.get(`/favorites/user/${currentUser}`)
+     axios.get(`/favorites/user/${currentUser}`)
     .then(result=>{
       this.setState({
         favorites:result.data.favorites
       })
     })
+
   }
 
+  postedButton=async()=>{
+    const{songs}=this.state
+
+    await this.setState({
+        selected:songs
+      })
+
+  }
+
+  favoriteButton=async()=>{
+    const{favorites}=this.state
+
+    await this.setState({
+        selected:favorites
+      })
+
+  }
+
+
   render (){
-    const{loggedInUser,currentUser}=this.state
+    const{loggedInUser,currentUser,selected}=this.state
     console.log(this.state)
 
     return(
       <div>
         <PostSongs loggedInUser={loggedInUser}currentUser={currentUser}/>
-        <button>Posted</button>
-        <button>Favorites</button>
+        <button onClick={this.postedButton}>Posted</button>
+        <button onClick={this.favoriteButton}>Favorites</button>
+        <DisplaySongs songs={selected} user={loggedInUser}/>
       </div>
     )
   }
